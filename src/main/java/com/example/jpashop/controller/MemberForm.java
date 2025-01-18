aaaf74c8d3b26897d3deace9db9aa25ae6ad1eac
ayz1070 <ayz1070@gmail.com>
2025-01-18 11:15:02 +0900

    [session8] 웹 계층 개발

--- src/main/java/com/example/jpashop/controller/MemberForm.java
+++ src/main/java/com/example/jpashop/controller/MemberForm.java
@@ -7,7 +7,7 @@ import lombok.Setter;
 @Getter @Setter
 public class MemberForm {
 
-    @NotEmpty(message = "회원 이름은 필수 입니다.")
+    @NotEmpty(message = "회원 이름은 필수값 입니다.")
     private String name;
 
     private String city;
