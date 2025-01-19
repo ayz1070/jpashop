d6af8dbcc9e5f2945618ed6a747fd98e076f5613
ayz1070 <ayz1070@gmail.com>
2025-01-19 11:12:42 +0900

    [session8] 웹 계층 개발

---
+++ src/main/java/com/example/jpashop/controller/ProductForm.java
@@ -0,0 +1,11 @@
+package com.example.jpashop.controller;
+
+public class ProductForm {
+    private Long id;
+    private String name;
+    private int price;
+    private int stockQuantity;
+    private String ingredient;
+
+
+}
