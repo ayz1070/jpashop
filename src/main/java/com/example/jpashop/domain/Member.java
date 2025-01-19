5bd948f0acfdfecbffa08af4116810028391c3dc
ayz1070 <ayz1070@gmail.com>
2025-01-19 22:22:19 +0900

    [jpa2 session2] API 개발 기본

--- src/main/java/com/example/jpashop/domain/Member.java
+++ src/main/java/com/example/jpashop/domain/Member.java
@@ -1,6 +1,7 @@
 package com.example.jpashop.domain;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -15,8 +16,10 @@ public class Member {
     @Column(name = "member_id")
     private Long id;
 
+    @NotEmpty
     private String name;
 
+    @Embedded
     private Address address;
 
     @OneToMany(mappedBy = "member")
