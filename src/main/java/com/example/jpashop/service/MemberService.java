5e331d464d7483629db5ba322f7229dedf629491
ayz1070 <ayz1070@gmail.com>
2025-01-20 21:59:21 +0900

    [jpa2 session2] API 개발 기본

--- src/main/java/com/example/jpashop/service/MemberService.java
+++ src/main/java/com/example/jpashop/service/MemberService.java
@@ -41,4 +41,10 @@ public class MemberService {
     public Member findOne(Long memberId) {
         return memberRepository.findOne(memberId);
     }
+
+    @Transactional(readOnly = true)
+    public void update(Long id, String name) {
+        Member member = memberRepository.findOne(id);
+        member.setName(name);
+    }
 }
